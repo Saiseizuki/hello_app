55f0e5cbdad09418cca3783240206523dc9ee141
xcptan <xcptan@gmail.com>
2015-10-22 07:00:34 +0000

    Added static pages with links

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  root 'pages#show', page: 'home'
+  
+  get 'goodbye' => 'application#goodbye'
+  get 'hello' => 'application#hello'
+  
+  get "/pages/:page" => "pages#show"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
